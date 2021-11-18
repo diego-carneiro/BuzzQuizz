5b7844d145632bca92a5c9dbd28133da04b1b18b
diego-carneio <diegosantos.cs@gmail.com>
2021-11-18 10:20:01 -0300

    trello: first task

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-
-// const pagInicial = document.querySelector(".corpo-pagina-inicial");
-// const telaCriação_1 = document.querySelector(".criacao_1");
-// const telaCriação_2 = document.querySelector(".criacao_2");
-// const telaCriação_3 = document.querySelector(".criacao_3");
-// const telaCriação_4 = document.querySelector(".criacao_4");
-
-// const paginaDeUmQuizz = document.querySelector('.pagina-de-um-quizz');
-
-
+function pageSwitch() {
+    
+    const page = document.querySelector(".hidden")
+    page.classList.remove("hidden")
+}
