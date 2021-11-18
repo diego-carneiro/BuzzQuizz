14d42bf8b5d8c09fadc8c3449c9451570b3f8988
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2021-11-18 18:16:41 -0300

    Listagem de quizes adicionada

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@ function pageSwitch() {
     const page = document.querySelector(".hidden")
     page.classList.remove("hidden")
 }
+function disporQuizes(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    promise.then(imprimirQuizes);
+    promise.catch();
+}
+function imprimirQuizes(resposta){
+    const listaQuizes = resposta.data;
+    const caixaQuizes = document.querySelector(".caixa-quizz");
+    console.log(listaQuizes);
+    listaQuizes.map(quiz =>
+        caixaQuizes.innerHTML += 
+        `<div class="quizzes-de-outros">
+        <img src="${quiz.image}"/>
+        <div class="sombra-imagem"></div>
+        <span>${quiz.title}</span>
+        </div>`
+        );
+}
