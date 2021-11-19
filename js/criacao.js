38b6dec173d4007f4b3fb92787f55f30c8ed85fa
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2021-11-19 20:44:12 -0300

    Função responsável por colapsar as perguntas adicionada, falta ajeitar o layout

--- js/criacao.js
+++ js/criacao.js
@@ -53,50 +53,64 @@ function criarTelaPerguntas(){
     let perguntas = "";
     for (let i = 0; i < infoBasica.answers.length; i++){
         perguntas +=
-        `<div class="dados-quizz_2">
-                <p class="input-title">Pergunta ${i+1}</p>
+        `<div class="dados-quizz_2 colapsavel" onclick="colapsarPergunta(this)">
+                <div class="conteudo-pergunta">
+                <label class="input-title">Pergunta ${i+1}</label>
                 <div class="input-box">
-                    <input placeholder="Texto da Pergunta" id="input-box" minlength="20" required>
-                    <input placeholder="Cor de fundo da pergunta" id="input-box" onchange="is_hexadecimal(this)" required>
+                    <input placeholder="Texto da Pergunta" name="titulo-pergunta-${i+1}" id="input-box" minlength="20" required>
+                    <input placeholder="Cor de fundo da pergunta" name="cor-pergunta-${i+1}" id="input-box" onchange="is_hexadecimal(this)" minlength="7" maxlength="7" value="#" required>
                 </div>
-                <p class="input-title">Resposta correta</p>
+                <label class="input-title">Resposta correta</label>
                 <div class="input-box">
-                    <input placeholder="Resposta correta" id="input-box" required>
-                    <input placeholder="URL da imagem" id="input-box" type="url" required>
+                    <input placeholder="Resposta correta" id="input-box" name="resposta-correta-${i+1}" required>
+                    <input placeholder="URL da imagem" id="input-box" type="url" name="url-resposta-correta-${i+1}" required>
                 </div>
-                <p class="input-title">Respostas Incorretas</p>
+                <label class="input-title">Respostas Incorretas</label>
                 <div class="input-box">
-                    <input placeholder="Resposta incorreta 1" id="input-box" required>
-                    <input placeholder="URL da imagem 1" id="input-box" type="url">
-                </div>
-                <div class="input-box">
-                    <input placeholder="Resposta incorreta 2" id="input-box">
-                    <input placeholder="URL da imagem 2" id="input-box" type="url">
+                    <input placeholder="Resposta incorreta 1" id="input-box" name="resposta-incorreta-${i+1}-1" required>
+                    <input placeholder="URL da imagem 1" id="input-box" type="url" name="url-resposta-incorreta-${i+1}-1" required>
                 </div>
                 <div class="input-box">
-                    <input placeholder="Resposta incorreta 3" id="input-box">
-                    <input placeholder="URL da imagem 3" id="input-box" type="url">
+                    <input placeholder="Resposta incorreta 2" id="input-box" name="resposta-incorreta-${i+1}-2">
+                    <input placeholder="URL da imagem 2" id="input-box" type="url" name="url-resposta-incorreta-${i+1}-1">
+                </div>
+                <div class="input-box">
+                    <input placeholder="Resposta incorreta 3" id="input-box" name="resposta-incorreta-${i+1}-3">
+                    <input placeholder="URL da imagem 3" id="input-box" type="url" name="url-resposta-incorreta-${i+1}-1">
+                </div>
                 </div>
             </div>`
     }
-    container.innerHTML =`<div class="conteudo-criacao">
-            <p class="instrucao">Crie suas perguntas</p>
-            ${perguntas}
-            <div class="botao-prosseguir" onclick="subirCriação_3()">
-                <p class="texto-botao-prosseguir">Prosseguir para criar níveis</p>
-            </div>
-            <div class="botao-voltar" onclick="subirCriação_1()">
-                <button class="voltar">Voltar</button>
-            </div>
-        </div>`
+    container.innerHTML =`  <form class="conteudo-criacao" name="perguntas-geral">
+                                <label class="instrucao">Crie suas perguntas</label>
+                                ${perguntas}
+                                <button type="submit" class="botao-prosseguir" onsubmit="subirCriação_3()">
+                                Prosseguir para criar níveis
+                                </button>
+                                <button type="button" class="botao-voltar" onclick="subirCriação_1()">
+                                Voltar
+                                </button>
+                            </form>`
 }
 function is_hexadecimal(elemento){
     let str = elemento.value;
-    regexp = /^[0-9a-fA-F]+$/;
+    regexp = /^[0-9a-fA-F#]+$/;
     if (regexp.test(str)) {
     }
     else {
-        str = "";
+        elemento.value = "";
         return alert("Insira uma cor hexadecimal");
+    }
+}
+
+function colapsarPergunta(elemento){
+    
+    elemento.classList.toggle("ativo");
+    let content = elemento.children[0];
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } 
+    else {
+        content.style.display = "block";
     }
 }
