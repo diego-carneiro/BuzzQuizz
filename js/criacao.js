3fcf3b455decc07767f2d35f73cfd126b756f263
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2021-11-20 17:18:04 -0300

    Perguntas inseridas agora são salvas no array de envio ao servidor

--- js/criacao.js
+++ js/criacao.js
@@ -1,5 +1,5 @@
 let quizUsuario = false;
-let infoBasica = {title: "", image: "", answers: [], levels: []};
+let infoBasica = {title: "", image: "", questions: [], levels: []};
 function criarTelaInicial(){
     const caixaBotao = document.querySelector(".criar-quizz");
     if (!quizUsuario) {
@@ -40,7 +40,7 @@ function verificarCriacao_1(){
     infoBasica = {
                  title: tituloQuizCriado, 
                  image: imagemQuizCriado, 
-                 answers: arrayPerguntas,
+                 questions: arrayPerguntas,
                  levels: arrayNiveis
                  };
     
@@ -51,7 +51,7 @@ function verificarCriacao_1(){
 function criarTelaPerguntas(){
     const container = document.querySelector(".tela-perguntas");
     let perguntas = "";
-    for (let i = 0; i < infoBasica.answers.length; i++){
+    for (let i = 0; i < infoBasica.questions.length; i++){
         perguntas +=
             `<button type="button" class="dados-quizz_2 colapsavel" onclick="colapsarPergunta(this)">
             Pergunta ${i+1}
@@ -59,30 +59,30 @@ function criarTelaPerguntas(){
             </button>
                 <div class="conteudo-pergunta">
                     <div class="input-box">
-                        <input placeholder="Texto da Pergunta" name="titulo-pergunta-${i+1}" id="input-box" minlength="20" required>
-                        <input placeholder="Cor de fundo da pergunta" name="cor-pergunta-${i+1}" id="input-box" onchange="is_hexadecimal(this)" minlength="7" maxlength="7" value="#" required>
+                        <input placeholder="Texto da Pergunta" name="titulo-pergunta" id="input-box" minlength="20" required>
+                        <input placeholder="Cor de fundo da pergunta" name="cor-pergunta" id="input-box" onchange="is_hexadecimal(this)" minlength="7" maxlength="7" value="#" required>
                     </div>
                     <label class="input-title">Resposta correta</label>
                     <div class="input-box">
-                        <input placeholder="Resposta correta" id="input-box" name="resposta-correta-${i+1}" required>
-                        <input placeholder="URL da imagem" id="input-box" type="url" name="url-resposta-correta-${i+1}" required>
+                        <input placeholder="Resposta correta" id="input-box" name="resposta-correta-${i}" required>
+                        <input placeholder="URL da imagem" id="input-box" type="url" name="url-resposta-correta-${i}" required>
                     </div>
                     <label class="input-title">Respostas Incorretas</label>
                     <div class="input-box">
-                        <input placeholder="Resposta incorreta 1" id="input-box" name="resposta-incorreta-${i+1}-1" required>
-                        <input placeholder="URL da imagem 1" id="input-box" type="url" name="url-resposta-incorreta-${i+1}-1" required>
+                        <input placeholder="Resposta incorreta 1" id="input-box" name="resposta-incorreta-1-${i}" required>
+                        <input placeholder="URL da imagem 1" id="input-box" type="url" name="url-resposta-incorreta-1-${i}" required>
                     </div>
                     <div class="input-box">
-                        <input placeholder="Resposta incorreta 2" id="input-box" name="resposta-incorreta-${i+1}-2">
-                        <input placeholder="URL da imagem 2" id="input-box" type="url" name="url-resposta-incorreta-${i+1}-1">
+                        <input placeholder="Resposta incorreta 2" id="input-box" name="resposta-incorreta-2-${i}">
+                        <input placeholder="URL da imagem 2" id="input-box" type="url" name="url-resposta-incorreta-2-${i}">
                     </div>
                     <div class="input-box">
-                        <input placeholder="Resposta incorreta 3" id="input-box" name="resposta-incorreta-${i+1}-3">
-                        <input placeholder="URL da imagem 3" id="input-box" type="url" name="url-resposta-incorreta-${i+1}-1">
+                        <input placeholder="Resposta incorreta 3" id="input-box" name="resposta-incorreta-3-${i}">
+                        <input placeholder="URL da imagem 3" id="input-box" type="url" name="url-resposta-incorreta-3-${i}">
                     </div>
                 </div>`
     }
-    container.innerHTML =`  <form class="conteudo-criacao" name="perguntas-geral" onsubmit="criarTelaNiveis(); return false">
+    container.innerHTML =`  <form class="conteudo-criacao" name="perguntas-geral" id="form-perguntas" onsubmit="submeterPerguntas(); criarTelaNiveis(); return false">
                                 <label class="instrucao">Crie suas perguntas</label>
                                 ${perguntas}
                                 <button type="submit" class="botao-prosseguir">
@@ -91,7 +91,69 @@ function criarTelaPerguntas(){
                                 <button type="button" class="botao-voltar" onclick="subirCriação_1()">
                                 Voltar
                                 </button>
+                                </form>`
+}
+function criarTelaNiveis(){
+    const container = document.querySelector(".tela-niveis");
+    const perguntas = document.querySelector(".tela-perguntas");
+    container.classList.remove("hidden");
+    perguntas.classList.add("hidden");
+    let niveis = "";
+    for (let i = 0; i < infoBasica.levels.length; i++){
+        niveis += 
+            `<button type="button" class="dados-quizz_2 colapsavel" onclick="colapsarPergunta(this)">
+            Nível${i+1}
+            <ion-icon name="create-outline" class="pencil"></ion-icon>
+            </button>
+            <div class="conteudo-pergunta">
+                <input placeholder="Título do nível" id="input-box" type="text" minlength="10" required>
+                <input placeholder="% de acerto mínima" id="input-box" type="number" min="0" max="100" required>
+                <input placeholder="URL da imagem do nível" id="input-box" type="url" required>
+                <input placeholder="Descrição do nível" id="input-box" type="text" minlength="30">
+            </div>`
+    }
+
+    container.innerHTML=`   <form class="conteudo-criacao" name="niveis-geral" onsubmit="conferirPresença(); return false">
+                            <label class="instrucao">Agora, decida os níveis</label>
+                            ${niveis}
+                            <button type="submit" class="botao-prosseguir">
+                            Prosseguir para criar níveis
+                            </button>
+                            <button type="button" class="botao-voltar" onclick="subirCriação_1()">
+                            Voltar
+                            </button>
                             </form>`
+    
+}
+function submeterPerguntas(){
+    let perguntas = [];
+    let titulos = document.getElementsByName("titulo-pergunta");
+    let cores = document.getElementsByName("cor-pergunta");
+    let i = 0;
+    for (i = 0; i < titulos.length; i++){
+        perguntas.push({title: titulos[i].value, color: cores[i].value, answers: respostasSubmetidas(i)});
+    }
+    infoBasica.questions = perguntas;
+}
+function respostasSubmetidas(index_pergunta){
+    let retorno = []
+    const resposta_correta = document.getElementsByName(`resposta-correta-${index_pergunta}`)[0].value;
+    const url_resposta_correta = document.getElementsByName(`url-resposta-correta-${index_pergunta}`)[0].value;
+    const resposta_incorreta1 = document.getElementsByName(`resposta-incorreta-1-${index_pergunta}`)[0].value;
+    const url_resposta_incorreta1 = document.getElementsByName(`url-resposta-incorreta-1-${index_pergunta}`)[0].value;
+    const resposta_incorreta2 = document.getElementsByName(`resposta-incorreta-2-${index_pergunta}`)[0].value;
+    const url_resposta_incorreta2 = document.getElementsByName(`url-resposta-incorreta-2-${index_pergunta}`)[0].value;
+    const resposta_incorreta3 = document.getElementsByName(`resposta-incorreta-3-${index_pergunta}`)[0].value;
+    const url_resposta_incorreta3 = document.getElementsByName(`url-resposta-incorreta-3-${index_pergunta}`)[0].value;
+    retorno = [{text: resposta_correta, image: url_resposta_correta, isCorrectAnswer: true},
+               {text: resposta_incorreta1, image: url_resposta_incorreta1, isCorrectAnswer: false}];
+    if (resposta_incorreta2 !== ""){
+        retorno.push({text: resposta_incorreta2, image: url_resposta_incorreta2, isCorrectAnswer: false});
+    }
+    if (resposta_incorreta3 !== ""){
+        retorno.push({text: resposta_incorreta3, image: url_resposta_incorreta3, isCorrectAnswer: false});
+    };
+    return retorno;
 }
 function is_hexadecimal(elemento){
     let str = elemento.value;
